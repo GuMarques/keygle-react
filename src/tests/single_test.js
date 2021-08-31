716a95d250ff40e80e6c3fb89e4beb392570e288
Gustavo <gtooliveiramarques@gmail.com>
2021-08-30 23:30:34 -0300

    Adição do teste

---
+++ src/tests/single_test.js
@@ -0,0 +1,31 @@
+const {By,Key,Builder} = require("selenium-webdriver");
+require("chromedriver");
+
+async function test(){
+ 
+    var searchString = "The Witcher";
+
+    //To wait for browser to build and launch properly
+    let driver = await new Builder().forBrowser("chrome").build();
+
+     //To fetch http://google.com from the browser with our code.
+     await driver.get("http://localhost:3000");
+         
+     //To send a search query by passing the value in searchString.
+     await driver.findElement(By.className("form-control")).sendKeys(searchString);
+
+     await driver.findElement(By.className("btn-g2a")).click();
+
+     //Verify the page title and print it
+     var title = await driver.getTitle();
+     console.log('Title is:',title);
+
+     //It is always a safe practice to quit the browser after execution
+
+     console.log("Teste concluido");
+
+     await driver.quit();
+
+}
+
+test()
